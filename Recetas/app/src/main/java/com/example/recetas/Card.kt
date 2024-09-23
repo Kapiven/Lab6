07e3a9d45d1394dd2fc9986c8bbf5da3ad6e11dd
Alejandrosiaus <132409910+Alejandrosiaus@users.noreply.github.com>
2024-09-22 18:38:37 -0600

    Update Card.kt
    Codigo ingresado

--- Recetas/app/src/main/java/com/example/recetas/Card.kt
+++ Recetas/app/src/main/java/com/example/recetas/Card.kt
@@ -1,2 +1,75 @@
 package com.example.recetas
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun CongratulatoryPopup(navController: NavHostController, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray.copy(alpha = 0.5f)), // Fondo difuminado
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.White, shape = RoundedCornerShape(16.dp))
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "YOU DID IT!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF072A6C)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Let your friends know about it",
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                color = Color.Black
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(painter = painterResource(id = R.drawable.img_10), contentDescription = "Google")
+                Image(painter = painterResource(id = R.drawable.img_11), contentDescription = "Facebook")
+                Image(painter = painterResource(id = R.drawable.img_12), contentDescription = "Instagram")
+                Image(painter = painterResource(id = R.drawable.img_13), contentDescription = "Twitter")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewCongratulatoryPopup() {
+    CongratulatoryPopup(rememberNavController())
+}
